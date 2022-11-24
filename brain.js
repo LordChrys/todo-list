737101f19afa21ae961f3aa5f517911a4d94de17
LordChrys <49178040+LordChrys@users.noreply.github.com>
2022-11-25 07:50:44 +1300

    002
    Changed the template for user input to a better solution.
    
    Testing it out to understand issues around logging when requested by button

--- brain.js
+++ brain.js
@@ -1,42 +1,57 @@
 /*
-1. Enter new item
-2. User clicks button to submit new item
-3. New item gets added to list
-4. Delete any item
-5. Reset from 0
+This project needs to
+
+1. Add user entry to list
+2. Delete any item from list
+3. Clear all items from list 
 
 
 The how:
-Function/Event listener to add new item to list
 
-    - List item template
-    - 
+1. create template for new items
+2. add user input to template
+3. add new template to list
+
+Function that takes user entry and appends to list
+addnew  = {
+
+}
 
 */
 
-const sub = document.querySelector("submit");
-const list = document.querySelector("list");
-let input = document.querySelector("input").value;
-let items = document.querySelectorAll("list-item"); 
+const sub = document.querySelector("#submit");
+const list = document.querySelector("#list");
+let input = document.querySelector("#input").value;
+let items = document.querySelectorAll(".list-item"); 
 
 
-
-const addToList = newItem =>{
-    let html = `
-    <li class="list-item">
-        ${newItem} <span class="delete"> X </span>
+const itemTemp = (item)=>{
+    let newItem = 
+    `<li class="list-item">
+        ${item} <span class="delete"> X </span>
     </li>
-    `
-    list.innerHTML += html;
+    `;
+    list.innerHTML += newItem
 }
 
-sub.addEventListener('submit', addToList());
-
+sub.addEventListener('click', itemTemp(input));
 
 
 /*
-Issues:
+Issues & Fixes:
 
-Submit, not click for this button
+Query selectors need to be followed by their proper CSS selectors in the brackets (".classname"), ("#idname")
+
+Submit, not click for this button (Look into it to see difference)
+
+Another way to add new item without creating standard li:
+    let newItem = 
+    `<li class="list-item">
+        ${input} <span class="delete"> X </span>
+    </li>
+    `;
+
+element.innerHTML is used to change or add something to an HTML document
+in this case, "list" is the selected area
 
 */
